76adfe4bc5ca7a42e0a31e28bafe2b2f8f55c8eb
surajshahi5008-debug <surajshahi5008@gmail.com>
2026-08-30 01:51:32 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
