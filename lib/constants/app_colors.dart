81cf0f485eaa34d8fd8ce642f3e617f97fa10928
surajshahi5008-debug <surajshahi5008@gmail.com>
2026-09-01 11:22:11 +0530

    Create app_colors.dart

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBlue = Color(0xFF3F51B5);
+  static const Color primaryGreen = Color(0xFF4CAF50);
+  static const Color backgroundLight = Color(0xFFF3E5F5);
+  static const Color textDark = Color(0xFF212121);
+  static const Color errorRed = Color(0xFFD32F2F);
+}
