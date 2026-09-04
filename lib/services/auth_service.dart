d5f18e75687ee0733de5c78d378e09fe62230b46
surajshahi5008-debug <surajshahi5008@gmail.com>
2026-09-04 15:04:40 +0530

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,40 +3,34 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // इमेल र पासवर्डबाट साइन अप गर्ने र भेरिफिकेसन लिङ्क पठाउने
-  Future<UserCredential> signUpWithEmail(String email, String password) async {
-    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    await userCredential.user?.sendEmailVerification();
-    return userCredential;
+  Future<UserCredential?> signUpWithEmail(String email, String password) async {
+    return await _auth.createUserWithEmailAndPassword(email: email, password: password);
   }
 
-  // फोन नम्बर भेरिफिकेसनका लागि (SMS OTP पठाउने)
   Future<void> verifyPhoneNumber({
     required String phoneNumber,
-    required Function(String verificationId) onCodeSent,
-    required Function(FirebaseAuthException e) onVerificationFailed,
-    required Function(PhoneAuthCredential credential) onVerificationCompleted,
+    required void Function(PhoneAuthCredential) onVerificationCompleted,
+    required void Function(FirebaseAuthException) onVerificationFailed,
+    required void Function(String verificationId, int? resendToken) onCodeSent,
   }) async {
     await _auth.verifyPhoneNumber(
       phoneNumber: phoneNumber,
       verificationCompleted: onVerificationCompleted,
       verificationFailed: onVerificationFailed,
-      codeSent: (String verificationId, int? resendToken) {
-        onCodeSent(verificationId);
-      },
+      codeSent: onCodeSent,
       codeAutoRetrievalTimeout: (String verificationId) {},
     );
   }
 
-  // फोन ओटिपी प्रमाणिकरण (Verify OTP)
-  Future<UserCredential> signInWithPhoneCredential(String verificationId, String smsCode) async {
+  Future<UserCredential?> signInWithCredential(PhoneAuthCredential credential) async {
+    return await _auth.signInWithCredential(credential);
+  }
+
+  Future<UserCredential?> signInWithPhoneCredential(String verificationId, String smsCode) async {
     PhoneAuthCredential credential = PhoneAuthProvider.credential(
       verificationId: verificationId,
       smsCode: smsCode,
     );
-    return await _auth.signInWithCredential(credential);
+    return await signInWithCredential(credential);
   }
 }
