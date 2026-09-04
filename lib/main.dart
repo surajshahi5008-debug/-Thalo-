f378b856857c9e9e1fcc3bcb5167b82940c45865
surajshahi5008-debug <surajshahi5008@gmail.com>
2026-09-04 10:06:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'constants/app_colors.dart';
 import 'constants/app_strings.dart';
-import 'screens/login_screen.dart';
-import 'screens/register_screen.dart';
-import 'screens/home_screen.dart';
+import 'screens/auth_wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,20 +10,8 @@ void main() async {
   runApp(const ThaloApp());
 }
 
-class ThaloApp extends StatefulWidget {
-  const ThaloApp({Key? key}) : super(key: key);
-
-  @override
-  State<ThaloApp> createState() => _ThaloAppState();
-}
-
-class _ThaloAppState extends State<ThaloApp> {
-  String currentLang = 'ne';
-  String selectedDate = '२०८३/०५/१६';
-
-  void _handleNotificationTap(String? payload) {
-    debugPrint("Notification tapped with payload: $payload");
-  }
+class ThaloApp extends StatelessWidget {
+  const ThaloApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -34,68 +20,9 @@ class _ThaloAppState extends State<ThaloApp> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: AppColors.primaryBlue,
-        scaffoldBackgroundColor: AppColors.backgroundLight,
-        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => LoginScreen(
-              currentLang: currentLang,
-              onLanguageChanged: (lang) {
-                setState(() {
-                  currentLang = lang;
-                  if (lang == 'en') {
-                    selectedDate = '2026/09/02';
-                  } else {
-                    selectedDate = '२०८३/०५/१६';
-                  }
-                });
-              },
-              onNotificationTap: _handleNotificationTap,
-              onLoginSuccess: () {
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-              goToRegister: () {
-                Navigator.pushNamed(context, '/register');
-              },
-            ),
-        '/register': (context) => RegisterScreen(
-              currentLang: currentLang,
-              onLanguageChanged: (lang) {
-                setState(() {
-                  currentLang = lang;
-                  if (lang == 'en') {
-                    selectedDate = '2026/09/02';
-                  } else {
-                    selectedDate = '२०८३/०५/१६';
-                  }
-                });
-              },
-              onNotificationTap: _handleNotificationTap,
-              onRegisterSuccess: () {
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-              goToLogin: () {
-                Navigator.pop(context);
-              },
-            ),
-        '/home': (context) => HomeScreen(
-              currentLang: currentLang,
-              onLanguageChanged: (lang) {
-                setState(() {
-                  currentLang = lang;
-                });
-              },
-              onNotificationTap: _handleNotificationTap,
-              selectedDate: selectedDate,
-              onCalendarTap: () {
-                // क्यालेन्डर खोल्ने वा मिति छान्ने लजिक
-              },
-              onLogout: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-            ),
-      },
+      home: const AuthWrapper(),
     );
   }
 }
