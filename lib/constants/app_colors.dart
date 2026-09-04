ba01a4165124a5f481a2da477b2ad6101483036b
surajshahi5008-debug <surajshahi5008@gmail.com>
2026-09-04 09:56:15 +0530

    Update app_colors.dart

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryBlue = Color(0xFF3F51B5);
-  static const Color primaryGreen = Color(0xFF4CAF50);
-  static const Color backgroundLight = Color(0xFFF3E5F5);
-  static const Color textDark = Color(0xFF212121);
-  static const Color errorRed = Color(0xFFD32F2F);
+  static const Color primaryBlue = Color(0xFF1E88E5);
+  // थप रङहरू यहाँ थप्न सक्नुहुन्छ
 }
