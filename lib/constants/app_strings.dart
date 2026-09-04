d2498459b8cc95e53d34b929217771d5c3d5a593
surajshahi5008-debug <surajshahi5008@gmail.com>
2026-09-04 09:57:36 +0530

    Update app_strings.dart

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -1,7 +1,3 @@
 class AppStrings {
   static const String appName = 'Thalo';
-  
-  // सामान्य मेसेजहरू
-  static const String requiredField = 'यो क्षेत्र खाली राख्न पाइँदैन';
-  static const String somethingWentwrong = 'केही गडबड भयो, कृपया फेरि प्रयास गर्नुहोस्';
 }
